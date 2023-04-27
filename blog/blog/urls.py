5a2346bc262d716fb3a4881b25062d7fcfb23dad
Ziran Xiong <edsiward100@163.com>
2023-04-27 20:52:50 +0800

    '日志器配置'

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -17,6 +17,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import logging
+logger = logging.getLogger('django')
+
+from django.http import HttpResponse
+
+
+def log(request):
+
+
+
+    logger.info('info')
+
+    return HttpResponse('test')
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', log)
 ]
